bb2c1540f2b407570af16c80fe5755592f333223
Jad Joubran <joubran.jad@gmail.com>
2018-07-11 11:35:59 +0200

    Improve design and edge cases

--- api.js
+++ api.js
@@ -1,12 +1,17 @@
 const huejay = require("huejay");
 
+const getClient = config => {
+  const client = new huejay.Client({
+    host: config.ip,
+    username: config.username
+  });
+  return client;
+};
+
 module.exports = {
   routes: {
     get: {
       discover: (req, res) => {
-        // const appRoot = req.app.locals.appRoot;
-        // const config = req.app.locals.config;
-
         huejay
           .discover()
           .then(bridges => {
@@ -15,6 +20,15 @@ module.exports = {
           .catch(error => {
             console.log(`An error occurred: ${error.message}`);
           });
+      },
+      lights: (req, res) => {
+        const config = req.app.locals.config;
+
+        const client = getClient(config.hue);
+
+        client.lights.getAll().then(lights => {
+          return res.send({ lights });
+        });
       }
     },
     post: {
@@ -37,22 +51,54 @@ module.exports = {
           .create(user)
           .then(user => {
             const config = {
-              hue: {
-                ip: body.ip,
-                username: "aD1SP94kayZp9T3mjfawr7bFVQLdMI6qd0IgUAYj"
-              }
+              ip: body.ip,
+              username: "aD1SP94kayZp9T3mjfawr7bFVQLdMI6qd0IgUAYj",
+              presets: []
             };
 
             return res.send({ config });
           })
           .catch(error => {
             if (error instanceof huejay.Error && error.type === 101) {
-              return console.log(`Link button not pressed. Try again...`);
+              console.log(`Link button not pressed. Try again...`);
+              return res.send({
+                message: "Link button not pressed. Try again..."
+              });
             }
             console.log(error.stack);
+            return res.send({
+              message: "Could not connect to bridge. Try again..."
+            });
+          });
+      },
+      "set-light": (req, res) => {
+        const body = req.body;
+        if (!body || !body.id) {
+          return res.send(false);
+        }
+
+        const config = req.app.locals.config;
+
+        const client = getClient(config.hue);
+
+        client.lights
+          .getById(body.id)
+          .then(light => {
+            console.log(body.state);
+            for (const key of Object.keys(body.state)) {
+              console.log(key, body.state[key]);
+              light[key] = body.state[key];
+            }
+
+            return client.lights.save(light);
+          })
+          .then(light => {
+            console.log(`Updated light [${light.id}]`);
+            return res.send(true);
           })
           .catch(error => {
-            console.log(error.message);
+            console.log("Something went wrong");
+            console.log(error.stack);
           });
       }
     }
